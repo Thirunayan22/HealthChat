9270b4111128118616a8407e00b39c1d81be1cfc
Thirunayan <thirunayan22@gmail.com>
2020-10-10 19:25:06 +0530

    refactored source

--- app/src/main/java/com/project/healthchat/MainActivity.java
+++ app/src/main/java/com/project/healthchat/MainActivity.java
@@ -1,19 +1,8 @@
 package com.project.healthchat;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
-import com.project.healthchat.ui.main.SectionsPagerAdapter;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
