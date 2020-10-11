891821ae1003fa66fba475558087bbf690b1196d
Thirunayan <thirunayan22@gmail.com>
2020-10-11 21:28:27 +0530

    Added debugging feature

--- healthchat_api/healthchat_api.py
+++ healthchat_api/healthchat_api.py
@@ -40,6 +40,8 @@ def return_home():
 def chat_response():
   if(request.method == "POST"):
     text_message = [request.args.get('text')]
+    print("\nPassed value : ",text_message)
+    print("Passed value type : ",type(text_message))
 
     question_encodings = module.signatures['question_encoder'](
       tf.constant(preprocess_sentences(text_message))
